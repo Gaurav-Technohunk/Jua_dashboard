069812ab73d6e13ff931cb8d06054c03e8f617d2
gaurav <technohunk.119@gmail.com>
2025-11-06 21:07:06 +0530

    url updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-// export const environment = {
-//   production: true,
-//   baseUrl:'http://74.50.81.177'
-// };
+export const environment = {
+  production: true,
+  baseUrl:'http://69.164.245.164'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,  
   // baseUrl:'http://localhost:8080'
-  baseUrl:'http://69.164.245.164'
 };
 
 
